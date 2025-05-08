c6400b1ef697bf3821ad192c52b5a38b9aed68e6
suryaganesh1912 <suryaganesh2619@gmail.com>
2025-05-08 10:21:52 +0530

    Initial Django backend with PostgreSQL integration

--- myDjangoApp/myDjangoApp/settings.py
+++ myDjangoApp/myDjangoApp/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-xx^l9brdw1jxsrj9@=8ozc=-gvzak-(vt4ak#$+5oa)^qjfv=d'
-
+SECRET_KEY=os.getenv('SIGNING_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
